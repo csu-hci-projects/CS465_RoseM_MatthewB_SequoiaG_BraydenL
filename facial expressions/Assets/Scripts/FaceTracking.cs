fea9d6039300551cd393d49ca65424a9b03ecbf0
Matthew Buckman <buckmanm@colostate.edu>
2025-04-08 17:05:49 -0600

    Spin model with eyebrows (un-tested)

--- facial expressions/Assets/Scripts/FaceTracking.cs
+++ facial expressions/Assets/Scripts/FaceTracking.cs
@@ -1,20 +1,37 @@
+using System;
 using UnityEngine;
 
 public class FaceTracking : MonoBehaviour
 {
-    [SerializeField] public OVRFaceExpressions FacialExpressions;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Exposed Variables
+    [SerializeField] public OVRFaceExpressions FacialExpressions; // ref to face tracking component
+    [SerializeField] public OVRFaceExpressions.FaceExpression SpinLeft; // enums for Spin Left/Right 
+    [SerializeField] public OVRFaceExpressions.FaceExpression SpinRight;
+    [SerializeField] [Range(0, 1.0f)] public float Weight = 0.75f; // weight value for comparing face expressions
+    [SerializeReference] public GameObject cakeModel; // ref to cake model to rotate
+
     void Start()
     {
-        if (FacialExpressions == null) Debug.Log("FacialExpresion Object Refrence Valid");
+        if (FacialExpressions == null) throw new Exception("No Refrence To Face Tracking Component");
+        if (cakeModel == null) throw new Exception("No Refrence To Cake Model");
         if (FacialExpressions.isActiveAndEnabled) Debug.Log("Facial Expressions not Enabled");
-        if (FacialExpressions.didStart) Debug.Log("Start Called!");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (FacialExpressions.isActiveAndEnabled) Debug.Log(FacialExpressions.GetWeight(OVRFaceExpressions.FaceExpression.JawDrop));
+        if (FacialExpressions.isActiveAndEnabled) // check if face tracking is enabled
+        {
+            if (FacialExpressions.GetWeight(SpinLeft) >= Weight) // check turn left
+            {
+                Vector3 target = new Vector3(0f, 0.5f, 0);
+                cakeModel.transform.Rotate(target); // turn model left
+            }
+            else if (FacialExpressions.GetWeight(SpinRight) >= Weight) // check turn right
+            {
+                Vector3 target = new Vector3(0f, -0.5f, 0);
+                cakeModel.transform.Rotate(target); // turn model right
+            }
+        }
         
     }
 }
