5a3da04038a7c58f98f5e136799302c73f57f99b
Matthew Buckman <buckmanm@colostate.edu>
2025-04-15 17:05:55 -0600

    Refine Face&Head Rotate

--- facial expressions/Assets/Scripts/FaceAndHead.cs
+++ facial expressions/Assets/Scripts/FaceAndHead.cs
@@ -6,11 +6,12 @@ public class FaceAndHead : MonoBehaviour
     [SerializeField] public OVRFaceExpressions _FaceTrackingComponent;
     [SerializeField] public GameObject _CakeModel;
     [SerializeField] public GameObject _Camera;
-
+    [SerializeField] public GameObject _DrawPoint;
     [SerializeField] public OVRFaceExpressions.FaceExpression _RotateExpression;
     [SerializeField][Range(0f, 1f)] public float _Weight = 0.8f;
     private Vector3 _Orgin;
     private bool _Rotating = false;
+    private Vector3 _DrawPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,10 +32,12 @@ public class FaceAndHead : MonoBehaviour
                 if (!_Rotating) 
                 {
                     _Orgin = _Camera.transform.rotation.eulerAngles;
+                    _DrawPosition = _DrawPoint.transform.position;
                     _Rotating = true;
                 }
-                Vector3 rotate = (_Orgin - (_Camera.transform.rotation.eulerAngles)) * .01f;
-                _CakeModel.transform.Rotate(rotate.x, rotate.y, 0);
+                Vector3 rotate = (_Orgin - (_Camera.transform.rotation.eulerAngles)) * .1f;
+                _CakeModel.transform.Rotate(rotate.x, rotate.y, 0, Space.World);
+                Gizmos.DrawLine(_DrawPosition, _DrawPoint.transform.position);
             }
             else _Rotating = false;
         }
